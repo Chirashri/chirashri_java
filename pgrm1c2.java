192aa46187caef504d13e96b65c291e3d43b679b
Chirashri <154724641+Chirashri@users.noreply.github.com>
2025-05-17 21:39:25 +0530

    Add files via upload

---
+++ pgrm1c2.java
@@ -0,0 +1,29 @@
+package github1;
+
+import java.util.LinkedList;
+import java.util.Iterator;
+
+public class pgrm1c2 {
+    public static void main(String[] args) {
+        // Create a LinkedList and add some elements
+        LinkedList<String> fruits = new LinkedList<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Cherry");
+        fruits.add("Date");
+        fruits.add("Elderberry");
+
+        // Display the original list
+        System.out.println("Original LinkedList:");
+        System.out.println(fruits);
+
+        // Reverse iteration using descendingIterator()
+        System.out.println("\nIterating in reverse order:");
+        Iterator<String> reverseIterator = fruits.descendingIterator();
+
+        while (reverseIterator.hasNext()) {
+            String fruit = reverseIterator.next();
+            System.out.println(fruit);
+        }
+    }
+}
