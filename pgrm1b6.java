ebeb9a833aedba160fedadfa327c221a2a409e31
Chirashri <154724641+Chirashri@users.noreply.github.com>
2025-05-17 17:24:58 +0530

    pgrm1b6.java

--- pgrm1b6.java
+++ pgrm1b6.java
@@ -1,3 +1,5 @@
+/* 1b6 Write a java program for getting different colors through ArrayList interface and delete nth
+element from the ArrayList object by using remove by index*/
 package github1;
 import java.util.ArrayList;
 import java.util.Scanner;
