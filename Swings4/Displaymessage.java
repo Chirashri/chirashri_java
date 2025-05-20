61e09ca22f0d8f7ca7f51386c2f4f24ab39fa012
Chirashri <154724641+Chirashri@users.noreply.github.com>
2025-05-20 20:31:49 +0530

    Displaymessage.java

--- Swings4/Displaymessage.java
+++ Swings4/Displaymessage.java
@@ -6,8 +6,8 @@ import java.awt.Color;
 import java.awt.Font;
 //A simple Swing application.
 import javax.swing.*;
-class pgrm4a {
-	pgrm4a () {
+class Displaymessage {
+	Displaymessage () {
 //Create a new JFrame container.
 		JFrame jfrm = new JFrame("A Simple Swing Application");
 //jfrm.setFont("Serif",Font.BOLD,18);
@@ -30,7 +30,7 @@ public static void main(String args[]) {
 //Create the frame on the event dispatching thread.
 	SwingUtilities.invokeLater(new Runnable() {
 		public void run() {
-				new pgrm4a();
+				new Displaymessage();
 				}
 		}
 			);
